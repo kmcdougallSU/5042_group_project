ca39b3bcf9382fd68c44dff38497b56bf7f2b40a
nisotiya <nidhiisotiya@gmail.com>
2024-01-25 19:25:37 -0800

    Add login method

--- src/Client.java
+++ src/Client.java
@@ -1,69 +1,101 @@
 import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String[] args) throws InterruptedException
+    enum STATUS {
+        SUCCESS,
+        FAIL,
+    }
+    Socket socket;
+    BufferedReader read;
+    PrintWriter output;
+
+    public void startClient() throws IOException{
+        //Create socket connection
+        socket = new Socket("localhost", 12345);
+
+        //create print writer for sending login to server
+        output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+        // Create a Scanner object
+        Scanner myObj = new Scanner(System.in);
+
+        // Prompt for username
+        System.out.print("Enter username: ");
+
+        // Read user input
+        String username = myObj.nextLine();
+
+        //send username to server
+        output.println(username);
+
+        //prompt for password
+        System.out.print("Enter password: ");
+
+        String password = myObj.nextLine();
+
+        //send password to server
+        output.println(password);
+        output.flush();
+
+        //create Buffered reader for reading response from server
+        read = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        int errorCode = read.read();
+
+        //read response from server
+        String response = read.readLine();
+        System.out.println("\n" + response);
+
+        if(Character.getNumericValue(errorCode) == STATUS.SUCCESS.ordinal())
+        {
+            showMenu();
+        }
+        else
+        {
+            System.out.println("Please try again.");
+        }
+    }
+
+    public void showMenu()
     {
-        Socket socket = null;
-        InputStreamReader inputStraemReader = null;
-        OutputStreamWriter outputSreamReader = null;
-        BufferedReader bufferReader = null;
-        BufferedWriter bufferWriter = null;
+        // Create a Scanner object
+        Scanner scanner = new Scanner(System.in);
 
-        try{
-            socket = new Socket("localhost", 12345);
+        System.out.println("---Menu---");
+        System.out.println("""
+                    1. Logout
+                    """);
 
-            inputStraemReader = new InputStreamReader(socket.getInputStream());
-            outputSreamReader = new OutputStreamWriter(socket.getOutputStream());
-
-            bufferReader = new BufferedReader(inputStraemReader);
-            bufferWriter = new BufferedWriter(outputSreamReader);
-
-            Scanner scanner = new Scanner(System.in);
-
-            while (true)
+        int menuOption;
+        do {
+            System.out.print("Enter the menu option: ");
+            menuOption = scanner.nextInt();
+            if (menuOption != 1)
             {
-                System.out.print("Input: ");
-                String messageToSend = scanner.nextLine();
-                bufferWriter.write(messageToSend);
-                bufferWriter.newLine();
-                bufferWriter.flush();
-
-                System.out.println("Server: " + bufferReader.readLine());
-
-                if (messageToSend.equalsIgnoreCase("BYE"))
-                    break;
+                System.out.println("Menu option is invalid.");
             }
-        } catch (IOException e) {
+        } while (menuOption != 1);
+
+        output.println(menuOption);
+    }
+
+    public static void main(String[] args){
+        Client client = new Client();
+        try {
+            client.startClient();
+        }
+        catch (UnknownHostException e)
+        {
             e.printStackTrace();
-
-        } finally {
-            try {
-                if (socket != null)
-                {
-                    socket.close();
-                }
-                if (inputStraemReader != null)
-                {
-                    inputStraemReader.close();
-                }
-                if(outputSreamReader != null)
-                {
-                    outputSreamReader.close();
-                }
-                if (bufferReader != null)
-                {
-                    bufferReader.close();
-                }
-                if (bufferWriter != null)
-                {
-                    bufferWriter.close();
-                }
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
         }
     }
 }
+
--- src/Server.java
+++ src/Server.java
@@ -1,51 +1,96 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.EmptyStackException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
+    enum STATUS {
+        SUCCESS,
+        FAIL,
+    }
+    Map<String, String> userPassword;
+    ServerSocket serversocket;
+    Socket client;
+    BufferedReader input;
+    PrintWriter output;
+    String loggedInUser;
 
-    public static void main(String[] args) throws InterruptedException, IOException {
-        Socket socket = null;
-        InputStreamReader inputStraemReader = null;
-        OutputStreamWriter outputSreamReader = null;
-        BufferedReader bufferReader = null;
-        BufferedWriter bufferWriter = null;
-        ServerSocket serverSocket = null;
+    public void doSignup() {
+        userPassword = new HashMap<>();
+        userPassword.put("User1", "Password1");
+        userPassword.put("User2", "Password2");
+        userPassword.put("User3", "Password3");
+        userPassword.put("User4", "Password4");
+    }
 
-        serverSocket = new ServerSocket(12345);
+    public void start() throws IOException {
+        doSignup();
+        serversocket = new ServerSocket(12345);
+        System.out.println("Connection Starting on port:" + serversocket.getLocalPort());
 
-        while(true) {
+        while (true) {
+            //accept connection from client
+            client = serversocket.accept();
+
+            //open buffered reader for reading data from client
+            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            //open print writer for writing data to client
+            output = new PrintWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            System.out.println("Waiting for connection from client");
+
             try {
-                socket = serverSocket.accept();
-
-                inputStraemReader = new InputStreamReader(socket.getInputStream());
-                outputSreamReader = new OutputStreamWriter(socket.getOutputStream());
-
-                bufferReader = new BufferedReader(inputStraemReader);
-                bufferWriter = new BufferedWriter(outputSreamReader);
-
-                while (true) {
-                    String messageFromClient = bufferReader.readLine();
-
-                    System.out.println("Client: " + messageFromClient);
-
-                    bufferWriter.write("Message received.");
-                    bufferWriter.newLine();
-                    bufferWriter.flush();
-
-                    if (messageFromClient.equalsIgnoreCase("BYE"))
-                        break;
-                }
-
-                socket.close();
-                inputStraemReader.close();
-                outputSreamReader.close();
-                bufferWriter.close();
-                bufferReader.close();
-
-            } catch (IOException e) {
+                process();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void process() throws IOException {
+        login();
+        int menuOption = input.read();
+        if (menuOption == 1)
+        {
+            logout();
+        }
+    }
+
+    public void login() throws IOException {
+
+        String username = input.readLine();
+        System.out.println("username: " + username);
+
+        String password = input.readLine();
+        System.out.println("password: " + password);
+
+        if(userPassword.containsKey(username) && password.equals(userPassword.get(username)))
+        {
+            output.print(STATUS.SUCCESS.ordinal());
+            output.println("Welcome, " + username + "\n");
+            loggedInUser = username;
+        }
+        else
+        {
+            output.print(STATUS.FAIL.ordinal());
+            output.println("Login Failed");
+        }
+        output.flush();
+    }
+
+    public void logout() throws IOException {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
